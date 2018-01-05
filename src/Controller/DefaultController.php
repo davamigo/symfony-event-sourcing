6b5ec809b346cdaaab3e3f85bdda28d21d0e53da
David Amigo <david.amigo@privalia.com>
2018-01-05 13:15:18 +0100

    Added default controller and a homepage. Test to connect to RabbitMQ queuing system

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Default controller class
+ *
+ * @package App\Controller
+ * @Route("/")
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        $connection = $this->get('amqp.connection');
+
+        $channel = $connection->channel();
+
+        $channel->exchange_declare(
+            'app.events.x',
+            'fanout',
+            false,
+            true,
+            false
+        );
+
+        return $this->render('base.html.twig');
+    }
+}
