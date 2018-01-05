f0f57537e94ee38685bb8eaf92d4a7dc47ccc816
David Amigo <david.amigo@privalia.com>
2018-01-05 09:37:31 +0100

    Added Dockerfile, docker-compose.yml and some docker scripts for development purposes

---
+++ docker/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dockerfile="docker-compose.yml"
+dir=$(dirname $0)
+cd $dir
+
+echo -e "Starting $dockerfile..."
+
+docker-compose -f $dockerfile build
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+mkdir -p ../var/docker/mysql
+mkdir -p ../var/docker/mongo
+
+docker-compose -f $dockerfile up -d --remove-orphans
